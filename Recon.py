d5d55128a19106c87976cc57e5ad2f1e626720f7
aqra <muhamad_aqra@connect.ite.edu.sg>
2021-08-16 11:10:42 +0800

    Spider Crawler v1.1 Successful Crawl on page, but with a PC Browser User-Agent

--- Recon.py
+++ Recon.py
@@ -1,9 +1,14 @@
 import scrapy
-
+from scrapy.http.request import Request
 
 class NewSpider(scrapy.Spider):
     name = "new_spider"
     start_urls = ['http://brickset.com/sets/year-2019'] # Website to crawl
+
+    def start_requests(self):
+        headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:48.0) Gecko/20100101 Firefox/48.0'} # User-Agent, change this as needed
+        for url in self.start_urls:
+            yield Request(url, headers=headers)
 
     def parse(self, response):
         css_selector = 'img'
