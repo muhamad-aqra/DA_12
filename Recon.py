60c89833d7bd8ffc3782a9056fb0a883b29b2ce9
aqra <muhamad_aqra@connect.ite.edu.sg>
2021-08-16 11:33:45 +0800

    Spider Crawler v1.2 Successful Crawl on page, now with a Mobile Browser User-Agent and User-Agent display on output.

--- Recon.py
+++ Recon.py
@@ -6,7 +6,7 @@ class NewSpider(scrapy.Spider):
     start_urls = ['http://brickset.com/sets/year-2019'] # Website to crawl
 
     def start_requests(self):
-        headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:48.0) Gecko/20100101 Firefox/48.0'} # User-Agent, change this as needed
+        headers = {'User-Agent': 'Mozilla/5.0 (Android 7.0; Mobile; rv:54.0) Gecko/54.0 Firefox/54.0'} # User-Agent, change this as needed
         for url in self.start_urls:
             yield Request(url, headers=headers)
 
@@ -17,3 +17,4 @@ class NewSpider(scrapy.Spider):
             yield {
                 'Image Link': x.xpath(newsel).extract_first(),
             }
+        print(response.request.headers['User-Agent']) #Display the User-Agent in the output
